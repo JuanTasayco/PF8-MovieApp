f98793ac2bccc9585922dc223e506f3bc6444be6
Juan Tasayco <juantasayco266@gmail.com>
2023-11-22 12:51:35 -0500

    addRating

--- src/app/shared/rating/rating.component.ts
+++ src/app/shared/rating/rating.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -6,8 +6,27 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './rating.component.html',
-  styleUrl: './rating.component.scss'
+  styleUrl: './rating.component.scss',
 })
-export class RatingComponent {
+export class RatingComponent implements OnInit {
+  @Input({
+    required: true,
+  })
+  numberRating!: number;
 
+  stars = signal<string[]>(['o', 'o', 'o', 'o', 'o']);
+
+  ngOnInit(): void {
+    this.setRating();
+  }
+
+  setRating() {
+    if (this.numberRating <= 5) {
+      this.stars.update(() =>
+        this.stars().map((item, index) =>
+          index < this.numberRating ? 'x' : 'o'
+        )
+      );
+    }
+  }
 }
